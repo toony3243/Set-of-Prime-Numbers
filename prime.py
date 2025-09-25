f2d864829b59eb0e4535772a43fa267f60a074a4
Lumin <t.ho.zheng@gmail.com>
2025-09-25 13:17:01 -0400

    enjoy

---
+++ prime.py
@@ -0,0 +1,24 @@
+import math
+with(open('prime.txt','r+')) as f:
+    primeSet = f.read().splitlines()
+    primeSetInt = [int(i) for i in primeSet]
+    del primeSet
+
+    curNum = primeSetInt[-1]
+    print("last number recorded was: " + str(curNum))
+    while True:
+        curNum += 2
+        print("currently testing: " + str(curNum))
+        isPrime = True
+        for i in primeSetInt:
+            if i > math.floor(math.sqrt(curNum)):
+                break
+            if curNum % 5 == 0 or curNum % i == 0:
+                isPrime = False
+                print("this number is not prime: " + str(curNum))
+                break
+        if isPrime:
+            f.write(str(curNum) + '\n')
+            primeSetInt.append(curNum)
+            print("this number is prime: " + str(curNum))
+            print("size of set is now: " + str(len(primeSetInt)))
