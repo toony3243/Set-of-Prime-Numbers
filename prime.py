fc2f2f6b107fcabbc13bf14fa17e74fe6ceab3aa
Lumin <t.ho.zheng@gmail.com>
2025-09-26 17:18:42 -0400

    Update prime.py
    better optimised

--- prime.py
+++ prime.py
@@ -8,21 +8,17 @@ with(open('prime.txt','r+')) as f:
     print("last number recorded was: " + str(curNum))
     while True:
         curNum += 2
-        print("currently testing: " + str(curNum))
+        if curNum % 3 == 0 or curNum % 5 == 0 or math.sqrt(curNum) == math.floor(math.sqrt(curNum)):
+            continue
         isPrime = True
-        for i in primeSetInt:
-            if math.sqrt(curNum) == math.floor(math.sqrt(curNum)):
-                isPrime == False
-                break
+        for i in range(3, len(primeSetInt)):
             if i > math.floor(math.sqrt(curNum)):
                 break
-            if curNum % 5 == 0 or curNum % i == 0:
+            if curNum % i == 0:
                 isPrime = False
-                print("this number is not prime: " + str(curNum))
                 break
         if isPrime:
             f.write(str(curNum) + '\n')
             primeSetInt.append(curNum)
             print("this number is prime: " + str(curNum))
-
             print("size of set is now: " + str(len(primeSetInt)))
